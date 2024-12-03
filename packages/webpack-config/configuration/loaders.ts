6e6c09b6197008fc9c5c6d3db0523944d09530f2
kwsv <kuptsov.valya@gmail.com>
2024-12-03 08:04:29 +0300

    create project

---
+++ packages/webpack-config/configuration/loaders.ts
@@ -0,0 +1,42 @@
+import type { LoadersConfigType, ConfigProps } from '../types';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshTypeScript from 'react-refresh-typescript';
+
+const LoadersConfig = ({ isDev }: ConfigProps): LoadersConfigType => {
+    const cssLoaders = {
+        test: /\.css$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+        ],
+    };
+
+    const assetsLoader = {
+        test: /\.(jpg|png|svg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: `assets/images/${isDev ? '[name][ext]' : '[hash][ext][query]'}`,
+        },
+    };
+
+    const tsLoader = {
+        test: /\.tsx?$/,
+        use: {
+            loader: 'ts-loader',
+            options: {
+                getCustomTransformers: () => {
+                    return {
+                        before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+                    };
+                },
+                transpileOnly: isDev,
+            },
+        },
+        exclude: /node_modules/,
+    };
+
+    return [tsLoader, cssLoaders, assetsLoader];
+};
+
+export default LoadersConfig;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+
+const root = document.getElementById('root') as HTMLElement;
+
+if (!root) {
+    throw Error('root not found');
+}
+
+const react = createRoot(root);
+
+react.render(<h1>APP</h1>);
